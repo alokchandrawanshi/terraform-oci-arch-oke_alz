3de3a74740091354d163dd6b8b1962f24e9c2739
alokchandrawanshi <106600437+alokchandrawanshi@users.noreply.github.com>
2023-07-10 16:02:14 +0530

    Update oke.tf

--- examples/oke-fully-public-no-existing-network/oke.tf
+++ examples/oke-fully-public-no-existing-network/oke.tf
@@ -2,7 +2,7 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 module "oci-oke" {
-  source                        = "github.com/oracle-devrel/terraform-oci-arch-oke"
+  source                        = "github.com/alokchandrawanshi/terraform-oci-arch-oke_alz"
   tenancy_ocid                  = var.tenancy_ocid
   compartment_ocid              = var.compartment_ocid
   use_existing_vcn              = false
